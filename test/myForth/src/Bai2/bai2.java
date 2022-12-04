11dd64e7ac7352b17a373f80a7500328844fa232
NGuyen Duc <91659267+zxmclzxmcl@users.noreply.github.com>
2022-12-04 23:00:37 +0700

    Add files via upload

--- test/myForth/src/Bai2/bai2.java
+++ test/myForth/src/Bai2/bai2.java
@@ -2,19 +2,18 @@ package Bai2;
 
 import java.util.Scanner;
 
+
 public class bai2 {
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     System.out.println("Nhap xau ky tu: ");
     String str = scanner.nextLine();
-    System.out.println("\nNhap ky tu can tim trong xau: ");
-    char c = scanner.next().charAt(0);
     int bodem = 0;
-    for (int i=0; i<str.length();i++){
-        if(str.charAt(i) == c){
-            bodem+=1;
+        for(int i = 0; i<str.length()-2; i++){
+            if(str.charAt(i) == 'a' && str.charAt(i+1) == 'b' && str.charAt(i+2) == 'c')    
+                bodem = bodem +1;
         }
-    }
+
     System.out.println("So lan ky tu can tim trong xau xuat hien la: ");
     System.out.println(bodem);
     scanner.close();
