1cfd14b6434101c829f254dd637f6b75f31d37e9
NGuyen Duc <91659267+zxmclzxmcl@users.noreply.github.com>
2022-12-08 14:38:30 +0700

    Add files via upload

--- test/myForth/src/Bai78/bai78.java
+++ test/myForth/src/Bai78/bai78.java
@@ -1,20 +1,59 @@
 package Bai78;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class bai78 {
+
+    public static boolean tendem(String name){
+        var words = name.split(" ");
+        int i=0;
+        while(i<words.length-1){
+            i++;
+        }
+        if(words.length<2){
+            return false; 
+        }
+        else if(i==2 && words[i-1].contains("Thi")){
+            return true;
+            }
+        else if (i==3 && words[i-1].contains("Thi")||words[i-2].contains("Thi")){
+            return true;
+            }
+        
+        else return false;
+}
+    public static boolean ten(String name){
+        var words = name.split(" ");
+        int i=0;
+        while(i<words.length-1){
+            i++;
+        }
+        if(words[i].contains("An")){
+            return true;
+        }
+        else return false;
+
+    }
     public static void main(String[] args) {
-        String str[]= {"Nguyen Minh Duc", "Vu Thi An", "Nguyen Duc An", "Tran Thi Thu Hien"};
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String str[]= new String[n];
         int sobanAn = 0;
         int sobanThi = 0;
-        for(int i=0; i<str.length; i++){
-            if(str[i].contains("An"))
-                sobanAn+=1;
-            if(str[i].contains("Thi"))
+        scanner.nextLine();
+        for(int i=0; i<n; i++){
+            str[i] = scanner.nextLine();
+            boolean a = ten(str[i]);
+            boolean b = tendem(str[i]);
+            if(a==true){
+                sobanAn +=1;
+            }
+            if(b==true){
                 sobanThi+=1;
+            }
         }
         System.out.println("So ban co ten la An: "+sobanAn);
         System.out.println("\nSo ban co ten dem la Thi: "+sobanThi);
+        scanner.close();
     }
 }
